bf5e1f252505138e77bc7aa5ea9089b466e92148
Brendan Keane <btkeane@gmail.com>
2025-03-17 23:49:41 -0700

    wip

--- e2e/terraform/prod/main.tf
+++ e2e/terraform/prod/main.tf
@@ -94,14 +94,14 @@ module "hub" {
         "MONAD_IMAGE" = "bkeane/monad/echo"
       },
       {
-        "MONAD_CHDIR" = "e2e/stage/go",
+        "MONAD_CHDIR" = "e2e/go",
         "MONAD_IMAGE" = "bkeane/monad/go",
       }
     ]
 
     deployments = [
       {
-        "MONAD_CHDIR" = "e2e/stage/go",
+        "MONAD_CHDIR" = "e2e/go",
         "MONAD_IMAGE" = "bkeane/monad/go",
         "MONAD_SERVICE" = "go",
       },
