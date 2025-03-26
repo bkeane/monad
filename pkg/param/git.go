5710b0b568e21bef3816bbdcdfe2aa6ca3364f2f
Brendan Keane <btkeane@gmail.com>
2025-03-26 09:24:26 -0700

    Verify correction on branch delete

--- pkg/param/git.go
+++ pkg/param/git.go
@@ -11,13 +11,11 @@ import (
 
 type Git struct {
 	cwd        string `arg:"-" json:"-"`
-	Chdir      string `arg:"--chdir,env:MONAD_CHDIR" placeholder:"path" default:"."`
-	Owner      string `arg:"--owner,env:MONAD_OWNER" placeholder:"name" default:"github.com/<owner>/repo.git"`
-	Repository string `arg:"--repo,env:MONAD_REPO" placeholder:"name" default:"github.com/owner/<repo>.git"`
-	Branch     string `arg:"--branch,env:MONAD_BRANCH" placeholder:"name" default:"current git branch"`
-	Sha        string `arg:"--sha,env:MONAD_SHA" placeholder:"sha" default:"current git sha"`
-	// Service    string `arg:"--service,env:MONAD_SERVICE" placeholder:"name" default:"current directory name"`
-	// ImagePath  string `arg:"--image,env:MONAD_IMAGE" placeholder:"path" default:"${owner}/${repo}/${service}"`
+	Chdir      string `arg:"--chdir,env:MONAD_CHDIR" placeholder:"path" help:"change working directory" default:"."`
+	Owner      string `arg:"--owner,env:MONAD_OWNER" placeholder:"name" help:"git repository owner" default:"github.com/<owner>/repo.git"`
+	Repository string `arg:"--repo,env:MONAD_REPO" placeholder:"name" help:"git repository name" default:"github.com/owner/<repo>.git"`
+	Branch     string `arg:"--branch,env:MONAD_BRANCH" placeholder:"name" help:"git repository branch" default:"current git branch"`
+	Sha        string `arg:"--sha,env:MONAD_SHA" placeholder:"sha" help:"git repository sha" default:"current git sha"`
 }
 
 func (g *Git) Validate() error {
@@ -55,14 +53,6 @@ func (g *Git) Validate() error {
 		g.Repository = git.Repo
 	}
 
-	// if g.Service == "" {
-	// 	git, err := git.Parse(g.cwd)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	g.Service = git.BasePath
-	// }
-
 	if g.Branch == "" {
 		git, err := git.Parse(g.cwd)
 		if err != nil {
@@ -78,10 +68,6 @@ func (g *Git) Validate() error {
 		}
 		g.Sha = git.Sha
 	}
-
-	// if g.ImagePath == "" {
-	// 	g.ImagePath = fmt.Sprintf("%s/%s/%s", g.Owner, g.Repository, g.Service)
-	// }
 
 	log.Info().
 		Str("cwd", g.cwd).
