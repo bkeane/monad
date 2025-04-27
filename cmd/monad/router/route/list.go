2225579bc2426c5de8e94aca1ed6cb70310a18b7
Brendan Keane <btkeane@gmail.com>
2025-04-26 19:31:36 -0700

    list ink

--- cmd/monad/router/route/list.go
+++ cmd/monad/router/route/list.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/bkeane/monad/pkg/param"
 	"github.com/bkeane/monad/pkg/state"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type List struct {
 	param.Aws   `arg:"-"`
-	AllBranches bool `arg:"--all"`
+	AllMonad    bool `arg:"--all-monads"`
+	AllBranches bool `arg:"--all-branches"`
 }
 
 func (l *List) Route(ctx context.Context, r Root) error {
@@ -24,16 +26,48 @@ func (l *List) Route(ctx context.Context, r Root) error {
 	}
 
 	var filtered []*param.StateMetadata
-	// match := lipgloss.NewStyle().Foreground(lipgloss.Color("150")).Render
-	for _, service := range services {
-		if service.Owner == l.Git.Owner {
-			if service.Repo == l.Git.Repository {
-				if service.Branch == l.Git.Branch {
-					filtered = append(filtered, service)
-				} else if l.AllBranches {
-					filtered = append(filtered, service)
+	if l.AllMonad {
+		filtered = services
+	} else {
+		for _, service := range services {
+			if service.Owner == l.Git.Owner {
+				if service.Repo == l.Git.Repository {
+					if service.Branch == l.Git.Branch {
+						filtered = append(filtered, service)
+					} else if l.AllBranches {
+						filtered = append(filtered, service)
+					}
 				}
 			}
+		}
+	}
+
+	// mutate
+	for _, service := range filtered {
+		service.Sha = safeShorten(service.Sha)
+	}
+
+	// highlight contextual matches
+	ink := lipgloss.NewStyle().Foreground(lipgloss.Color("150")).Render
+	for _, service := range filtered {
+		if service.Service == l.Service.Name {
+			service.Service = ink(service.Service)
+		}
+
+		if service.Owner == l.Git.Owner {
+			service.Owner = ink(service.Owner)
+		}
+
+		if service.Repo == l.Git.Repository {
+			service.Repo = ink(service.Repo)
+		}
+
+		if safeShorten(service.Sha) == safeShorten(l.Git.Sha) {
+			service.Sha = ink(service.Sha)
+		}
+
+		if service.Branch == l.Git.Branch {
+			service.Branch = ink(service.Branch)
 		}
 	}
 
@@ -42,4 +76,11 @@ func (l *List) Route(ctx context.Context, r Root) error {
 	}
 
 	return nil
+}
+
+func safeShorten(sha string) string {
+	if len(sha) > 7 {
+		return sha[:7]
+	}
+	return sha
 }
