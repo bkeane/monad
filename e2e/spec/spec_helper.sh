2909ca7191bf1e3089148bab6424ef00726d68de
Brendan Keane <btkeane@gmail.com>
2025-04-29 08:52:22 -0700

    force default region in test

--- e2e/spec/spec_helper.sh
+++ e2e/spec/spec_helper.sh
@@ -12,6 +12,7 @@ spec_helper_precheck() {
   # Available functions: info, warn, error, abort, setenv, unsetenv
   # Available variables: VERSION, SHELL_TYPE, SHELL_VERSION
   setenv LOG_LEVEL=warn
+  setenv AWS_DEFAULT_REGION=us-west-2
   setenv MONAD_REPO=${MONAD_REPO:=monad}
   setenv MONAD_BRANCH=${MONAD_BRANCH:=$(git rev-parse --abbrev-ref HEAD)}
   setenv MONAD_SHA=${MONAD_SHA:=$(git rev-parse HEAD)}
