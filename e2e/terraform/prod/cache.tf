2c32eb431be1f1478242b1275ee4605a9617ede6
Brendan Keane <btkeane@gmail.com>
2025-05-25 11:11:28 -0700

    fixup releases

--- e2e/terraform/prod/cache.tf
+++ e2e/terraform/prod/cache.tf
@@ -1,27 +1,27 @@
-# resource "aws_s3_bucket" "cache" {
-#   bucket = "kaixo-buildx-cache"
-# }
+resource "aws_s3_bucket" "cache" {
+  bucket = "kaixo-buildx-cache"
+}
 
-# resource "aws_s3_bucket_policy" "cache" {
-#   bucket = aws_s3_bucket.cache.id
+resource "aws_s3_bucket_policy" "cache" {
+  bucket = aws_s3_bucket.cache.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid       = "AllowFullAccess"
-#         Effect    = "Allow"
-#         Principal = {
-#           AWS = module.topology.integration.role_arn
-#         }
-#         Action = [
-#           "s3:*"
-#         ]
-#         Resource = [
-#           aws_s3_bucket.cache.arn,
-#           "${aws_s3_bucket.cache.arn}/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowFullAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = module.topology.resources["prod"]["build"].role_arn
+        }
+        Action = [
+          "s3:*"
+        ]
+        Resource = [
+          aws_s3_bucket.cache.arn,
+          "${aws_s3_bucket.cache.arn}/*"
+        ]
+      }
+    ]
+  })
+}
