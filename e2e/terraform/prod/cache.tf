47936ef10b4fd513ce44334af1ec859efefda41b
Brendan Keane <btkeane@gmail.com>
2025-05-26 10:34:15 -0700

    add action ref to docs

--- e2e/terraform/prod/cache.tf
+++ e2e/terraform/prod/cache.tf
@@ -2,6 +2,27 @@ resource "aws_s3_bucket" "cache" {
   bucket = "kaixo-buildx-cache"
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "cache" {
+  bucket = aws_s3_bucket.cache.id
+
+  rule {
+    id     = "cache-expiry"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    expiration {
+      days = 30
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "cache" {
   bucket = aws_s3_bucket.cache.id
 
