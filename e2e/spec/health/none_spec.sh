853a9191d6bbb9b8ebee4e78cce3a6e33f2ec49f
Brendan Keane <btkeane@gmail.com>
2025-07-30 20:35:15 -0700

    add e2e

--- e2e/spec/health/none_spec.sh
+++ e2e/spec/health/none_spec.sh
@@ -1,6 +1,7 @@
 Describe "Health (no auth)"
+  target="https://${MONAD_HOST}/${MONAD_REPO}/${MONAD_BRANCH}/${MONAD_SERVICE}/public/health"
   It "${MONAD_SERVICE}"
-    When call curl_until 200 "https://${MONAD_HOST}/${MONAD_REPO}/${MONAD_BRANCH}/${MONAD_SERVICE}/health"
+    When call curl_until 200 "${target}"
     The output should include 200
     The status should be success
   End
